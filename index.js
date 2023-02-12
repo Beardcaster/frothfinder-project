fddb2a7875ff01f4fbcc6abc3404f5ff26b12e40
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-02-11 17:59:51 -0700

    functional per-page dropdown

--- index.js
+++ index.js
@@ -19,19 +19,19 @@ const submitForm = hook("search-form")
 submitForm.addEventListener('submit', e => {
     e.preventDefault();
     const query = searchBy.value; //value of dropdown to query to pass into get
+    const perPage = hook("per-page").value; //per page dropdown value for get
     const searchValue = e.target.item.value; //assigns string in form to search value  
-    initiateSearch(query, searchValue);   
+    initiateSearch(query, searchValue, perPage);   
 })
 
-
-function initiateSearch(query, searchValue) {
+function initiateSearch(query, searchValue, perPage) {
 
     if(searchValue.length === 0){
         console.log('no search value enterred') //without this an empty search returns 20 breweries from the front of the database.
         return;
     }
     
-    fetch(`https://api.openbrewerydb.org/breweries?${query}=${searchValue}`)
+    fetch(`https://api.openbrewerydb.org/breweries?${query}=${searchValue}&per_page=${perPage}`)
     .then(resp => resp.json())
     .then(result => {   
         hook("search-result").innerHTML = "";    
@@ -46,6 +46,19 @@ function renderResults(object) {
     const result = spawn("li")
     result.innerText = object.name
     hook("search-result").appendChild(result);
+
+    result.addEventListener('click', () => {
+        console.log(object)
+        const detailResults = hook('details');
+        detailResults.innerHTML = `
+        <h4>Name: ${object.name}</h4>
+        <p>Address: ${object.street}</p>
+        <p> ${object.city}, ${object.state} ${object.postal_code}</p>
+        <p>Country: ${object.country}</p>
+        <p>Phone Number: ${object.phone}</p>
+        <p>Brewery Type: ${object.brewery_type}</p>
+        `
+    })
 }
 
 function hook (id) {
